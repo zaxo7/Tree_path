8e132d2a6ff732d28eae5df25275f8d5c334d5a8
zaxo7 <zaxo.7@hotmail.com>
2018-11-02 17:03:29 +0100

    clean the files and functions

--- main.cpp
+++ main.cpp
@@ -6,41 +6,5 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	
-	List tree;
-
-	tree.read(); // read the tree from the file
-
-    tree.rest();
- 
-    tree.path('A','S');
-
-    cout << endl;
-    while(1);
-
-	/*int br;
-	string input;
-	while(1)
-	{
-		cin >> input >> br;
-		if(input == "print")
-		{
-			tree.print_curr();
-		}
-		else if(input =="forw")
-		{
-			tree.step_forw(br);
-			tree.print_curr();
-		}
-		else if(input == "back")
-		{
-			tree.step_back(br);
-			tree.print_curr();
-		}
-		else if(input == "rest")
-		{
-			tree.rest();
-		}
-
-	}*/
 	return 0;
 }
