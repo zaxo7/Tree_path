9a69022345f6cf42c057580a4ce82b7f9e20c7f7
zaxo.7 <zaxo.7@hotmail.com>
2018-11-05 18:39:07 +0100

    delete the switch

--- functions.cpp
+++ functions.cpp
@@ -1,65 +1,58 @@
 #include "functions.h"
 
 extern int arrive;
-extern bool rel[N][N] , b;
+extern char* paths;
+extern bool rel[N][N] , b,d;
 
 int* path_tab = NULL;
 
 void path(int depart)
 {
-	if(b)
+	static int k = 0; // pour que la variable sera pas effacer aprée le break ou return
+
+	if(b) // b sera vrée quand on trouve le chemin vers notre destination
 		return ;
-	switch(depart)
+	if(k == 0)
 	{
-		case A:
-		std::cout << "A > " ;
-		break;
-		case B:
-		std::cout << "B > " ;
-		break;
-		case C:
-		std::cout << "C > " ;
-		break;
-		case D:
-		std::cout << "D > " ;
-		break;
-		case E:
-		std::cout << "E > " ;
-		break;
-		case F:
-		std::cout << "F > " ;
-		break;
+		paths[0] = depart + 65; //transformer le int vs char
+		k++;
 	}
+	
+	// il faut pas metre une ville deux fois dans un chemin
+	d = false;
+	for(int i = 0; i < k; i++)
+	{
+		if(paths[i] == (char)(depart+65))
+			d = true;
+	}
+	if(!d) 
+	{
+		paths[k] = depart+65;
+		k++;
+	}
+
+	// si on trouve une autre relation en appelle again la function
 	for(int i = 0; i < N; i++)
 	{
 		if(rel[depart][arrive])
 		{
-			switch(arrive)
+			d = false;
+			for(int i = 0; i < k; i++)
 			{
-				case A:
-				std::cout << "A" ;
-				break;
-				case B:
-				std::cout << "B" ;
-				break;
-				case C:
-				std::cout << "C" ;
-				break;
-				case D:
-				std::cout << "D" ;
-				break;
-				case E:
-				std::cout << "E" ;
-				break;
-				case F:
-				std::cout << "F" ;
-				break;
+				if(paths[i] == (char)(depart+65))
+					d = true;
+			}
+			if(!d)
+			{
+				paths[k] = depart+65;
+				k++;
 			}
 			b++;
 			break;
 		}
 		if(rel[depart][i])
 		{
+			rel[depart][i] = 0;
 			path(i);
 		}
 	}
--- main.cpp
+++ main.cpp
@@ -3,8 +3,9 @@
 
 using namespace std;
 
-int arrive = D;
-bool b = false;
+int arrive = F;
+char* paths = new char[1];
+bool b = false,d = false;
 
 bool rel[N][N] = {
 		0, 1, 0, 0, 0, 0,
@@ -23,17 +24,26 @@ int main(int argc, char const *argv[])
 
 	char tmp;
 
-	cout << "Donner la ville de depart :";
+	/*cout << "Donner la ville de depart :";
     cin >> tmp;
     depart=tmp - 65;
 
     cout << "Donner la ville de d'arrive :";
 	cin >> tmp;
-	arrive=tmp - 65;
+	arrive=tmp - 65;*/
 
-	path(depart);
+	path(1);
+	// si b false il n ya pas de chemain 
 	if(!b)
 		cout << "no way ;D" << endl;
+
+	cout << paths[0] ;
+	int j = 1;
+	while(/*paths[j] >= 65 && paths[j] < 65+27*/j < 10)
+	{
+		cout << " > " << paths[j] ;
+		j++;
+	}
 
 
 
